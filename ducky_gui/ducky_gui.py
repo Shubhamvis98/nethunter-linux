abf9295fa4e9f74d40186ca5dabec7137d3dccc4
Shubham Vishwakarma <43336210+Shubhamvis98@users.noreply.github.com>
2023-02-28 18:30:08 +0530

    added some extra functions in gui

--- ducky_gui/ducky_gui.py
+++ ducky_gui/ducky_gui.py
@@ -1,35 +1,85 @@
 import gi, time, subprocess
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, Gio
+gi.require_version('Notify', '0.7')
+from gi.repository import Gtk, Gio, Notify
 import ducky
 
 
 class MainWindow:
     def __init__(self):
+        self.appname = 'Ducky Injector'
         self.builder = Gtk.Builder()
         self.builder.add_from_file('ducky.glade')
         self.mainWindow = self.builder.get_object('mainWindow')
-        self.mainWindow.set_title("Ducky Injector")
+        self.mainWindow.set_title(self.appname)
+        Notify.init(self.appname)
 
         self.editor = self.builder.get_object('editor')
+        self.btnOpen = self.builder.get_object('open')
         self.btnClear = self.builder.get_object('clear')
-        self.btnRun = self.builder.get_object('run')
+        self.btnSave = self.builder.get_object('save')
+        self.btnInject = self.builder.get_object('inject')
 
+        self.btnOpen.connect('clicked', self.openInEditor)
         self.btnClear.connect('clicked', self.clearEditor)
-        self.btnRun.connect('clicked', self.inject)
+        self.btnSave.connect('clicked', self.save)
+        self.btnInject.connect('clicked', self.inject)
+
+        self.buffer = self.editor.get_buffer()
 
         self.mainWindow.connect("destroy", Gtk.main_quit)
         self.mainWindow.show_all()
-
+    
     def clearEditor(self, btn):
         buffer = self.editor.get_buffer()
         buffer.set_text('')
 
     def inject(self, btn):
-        buffer = self.editor.get_buffer()
-        startIter, endIter = buffer.get_bounds()
-        TMP = buffer.get_text(startIter, endIter, False)
+        startIter, endIter = self.buffer.get_bounds()
+        TMP = self.buffer.get_text(startIter, endIter, False)
         ducky.inject_raw(TMP)
+    
+    def openInEditor(self, btn):
+        filechooser = Gtk.FileChooserDialog(title="Open Ducky", parent=None, action=Gtk.FileChooserAction.OPEN)
+        filechooser.add_buttons("_Open", Gtk.ResponseType.OK)
+        filechooser.add_buttons("_Cancel", Gtk.ResponseType.CANCEL)
+        filechooser.set_default_response(Gtk.ResponseType.OK)
+        response = filechooser.run()
+
+        if response == Gtk.ResponseType.OK:
+            try:
+                with open(filechooser.get_filename()) as f:
+                    self.buffer.set_text(f.read())
+            except TypeError:
+                buffer.set_text('Select File First...')
+        filechooser.destroy()
+    
+    def save(self, btn):
+        startIter, endIter = self.buffer.get_bounds()
+        TMP = self.buffer.get_text(startIter, endIter, False)
+        if TMP == '':
+            self.notification('Editor is empty')
+            return
+
+        filechooser = Gtk.FileChooserDialog(title="Open Ducky", parent=None, action=Gtk.FileChooserAction.SAVE)
+        filechooser.add_buttons("_Save", Gtk.ResponseType.OK)
+        filechooser.add_buttons("_Cancel", Gtk.ResponseType.CANCEL)
+        filechooser.set_default_response(Gtk.ResponseType.OK)
+        response = filechooser.run()
+
+        if response == Gtk.ResponseType.OK:
+            try:
+                with open(filechooser.get_filename(), 'w') as f:
+                    f.write(TMP)
+                    self.notification('File Saved')
+            except PermissionError:
+                self.notification('File Not Saved, !!!Access Denied!!!')
+        filechooser.destroy()
+
+    def notification(self, msg):
+        msg = Notify.Notification.new(self.appname, msg)
+        msg.show()
+
 
 win = MainWindow()
 Gtk.main()
