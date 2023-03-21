0e543fba2bdbc19dab812bffd4be66e71a06b9de
Shubham Vishwakarma <43336210+Shubhamvis98@users.noreply.github.com>
2023-03-21 18:20:43 +0530

    code improvement

--- nethunter.py
+++ nethunter.py
@@ -8,17 +8,25 @@ from gi.repository import Gtk, Gio, Notify
 from bin import ducky
 
 
-class MainWindow:
-    def __init__(self):
-        self.appname = 'NETHUNTER for PinePhone'
-        self.usbarsenal = './bin/usbarsenal'
-        self.builder = Gtk.Builder()
-        self.builder.add_from_file('nethunter.ui')
-        self.mainWindow = self.builder.get_object('mainWindow')
-        self.mainWindow.set_title(self.appname)
-        Notify.init(self.appname)
+class Functions:
+    def get_output(self, cmd):
+        run = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+        run.wait()
+        output = str(run.communicate()[0].decode())
+        returncode = run.poll()
+        return [output, returncode]
+    
+    def notification(self, msg):
+        try:
+            msg = Notify.Notification.new(msg)
+            msg.show()
+        except:
+            pass
 
-        # ARSENAL
+class Arsenal(Functions):
+    def __init__(self, builder):
+        self.builder = builder
+        self.usbarsenal = './bin/usbarsenal'
         self.function = self.builder.get_object('function')
         self.idVen = self.builder.get_object('idVen')
         self.idProd = self.builder.get_object('idProd')
@@ -34,141 +42,7 @@ class MainWindow:
 
         self.status_buffer = self.status.get_buffer()
         self.setDefault()
-
-        # DUCKY
-        self.editor = self.builder.get_object('editor')
-        self.btnOpen = self.builder.get_object('open')
-        self.btnClear = self.builder.get_object('clear')
-        self.btnSave = self.builder.get_object('save')
-        self.btnInject = self.builder.get_object('inject')
-
-        self.btnOpen.connect('clicked', self.openInEditor)
-        self.btnClear.connect('clicked', self.clearEditor)
-        self.btnSave.connect('clicked', self.save)
-        self.btnInject.connect('clicked', self.inject)
-
-        self.editor_buffer = self.editor.get_buffer()
-
-        # MAC Changer
-        self.maciface = self.builder.get_object('maciface')
-        self.currentmac = self.builder.get_object('currentmac')
-        self.newmac = self.builder.get_object('newmac')
-        self.btnranmac = self.builder.get_object('btn_ranmac')
-        self.btnchmac = self.builder.get_object('btn_chmac')
-        self.btnrstmac = self.builder.get_object('btn_rstmac')
-
-        self.maciface.connect('changed', self.on_iface_change)
-        self.btnranmac.connect('clicked', self.gen_random_mac)
-        self.btnchmac.connect('clicked', self.chmac)
-        self.btnrstmac.connect('clicked', self.reset_mac)
-        self.getifaces()
-
-        # START GUI
-        self.mainWindow.connect("destroy", Gtk.main_quit)
-        self.mainWindow.show_all()
-
-    # MAC CHANGER FUNCTIONS
-    def getifaces(self):
-        tmp = psutil.net_if_addrs()
-        iface_list = list(tmp.keys())
-        ifindex = 0 if 'wlan0' not in iface_list else iface_list.index('wlan0')
-        for iface in range(len(iface_list)):
-            self.maciface.append_text(iface_list[iface])
-        self.maciface.set_active(ifindex)
     
-    def getmac(self, iface):
-        tmp = psutil.net_if_addrs()
-        for i in range(len(tmp[iface])):
-            if 'AddressFamily.AF_PACKET' in str(tmp[iface][i]):
-                return tmp[iface][i][1]
-
-    def on_iface_change(self, iface):
-        curmac = self.getmac(iface.get_active_text())
-        self.currentmac.set_text(curmac)
-    
-    def gen_random_mac(self, btn):
-        mac = ':'.join((f'0{(hex(i)[2:])}' if i<16 else hex(i)[2:]) for i in [random.randint(0, 255) for i in range(6)])
-        self.newmac.set_text(mac)
-    
-    def chmac(self, btn):
-        ifname = self.maciface.get_active_text()
-        newmac = self.newmac.get_text()
-        if len(newmac) == 17:
-            self.get_output(f'ip link set {ifname} down')
-            out = self.get_output(f'macchanger -m {newmac} {ifname}')
-            self.get_output(f'ip link set {ifname} up')
-            if out[1] == 0:
-                self.notification('*MAC CHANGED: *')
-            else:
-                self.notification('!!! MAC NOT CHANGED !!!')
-        self.on_iface_change(self.maciface)
-
-    def reset_mac(self, btn):
-        ifname = self.maciface.get_active_text()
-        self.get_output(f'ip link set {ifname} down')
-        out = self.get_output(f'macchanger -p {ifname}')
-        self.get_output(f'ip link set {ifname} up')
-        if out[1] == 0:
-            self.notification('*MAC RESET DONE: *')
-        else:
-            self.notification('!!! MAC NOT CHANGED !!!')
-        self.on_iface_change(self.maciface)
-
-
-    # DUCKY FUNCTIONS
-    def clearEditor(self, btn):
-        self.editor_buffer.set_text('')
-
-    def inject(self, btn):
-        startIter, endIter = self.editor_buffer.get_bounds()
-        TMP = self.editor_buffer.get_text(startIter, endIter, False)
-        ducky.inject_raw(TMP)
-    
-    def openInEditor(self, btn):
-        filechooser = Gtk.FileChooserDialog(title="Open Ducky", parent=None, action=Gtk.FileChooserAction.OPEN)
-        filechooser.add_buttons("_Open", Gtk.ResponseType.OK)
-        filechooser.add_buttons("_Cancel", Gtk.ResponseType.CANCEL)
-        filechooser.set_default_response(Gtk.ResponseType.OK)
-        response = filechooser.run()
-
-        if response == Gtk.ResponseType.OK:
-            try:
-                with open(filechooser.get_filename()) as f:
-                    self.editor_buffer.set_text(f.read())
-            except TypeError:
-                buffer.set_text('Select File First...')
-        filechooser.destroy()
-    
-    def save(self, btn):
-        startIter, endIter = self.editor_buffer.get_bounds()
-        TMP = self.editor_buffer.get_text(startIter, endIter, False)
-        if TMP == '':
-            self.notification('Editor is empty')
-            return
-
-        filechooser = Gtk.FileChooserDialog(title="Open Ducky", parent=None, action=Gtk.FileChooserAction.SAVE)
-        filechooser.add_buttons("_Save", Gtk.ResponseType.OK)
-        filechooser.add_buttons("_Cancel", Gtk.ResponseType.CANCEL)
-        filechooser.set_default_response(Gtk.ResponseType.OK)
-        response = filechooser.run()
-
-        if response == Gtk.ResponseType.OK:
-            try:
-                with open(filechooser.get_filename(), 'w') as f:
-                    f.write(TMP)
-                    self.notification('File Saved')
-            except PermissionError:
-                self.notification('File Not Saved, !!!Access Denied!!!')
-        filechooser.destroy()
-
-    def notification(self, msg):
-        try:
-            msg = Notify.Notification.new(self.appname, msg)
-            msg.show()
-        except:
-            pass
-    
-    # ARSENAL FUNCTIONS
     def getMassStoragePath(self):
         self.filechooser = self.builder.get_object('filechooser')
         return self.filechooser.get_filename()
@@ -231,13 +105,155 @@ class MainWindow:
     def disable(self, btn):
         self.setStatus(self.get_output(f'{self.usbarsenal} -d')[0])
 
-    def get_output(self, cmd):
-        run = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-        run.wait()
-        output = str(run.communicate()[0].decode())
-        returncode = run.poll()
-        return [output, returncode]
+class Ducky(Functions):
+    def __init__(self, builder):
+        self.builder = builder
+        self.editor = self.builder.get_object('editor')
+        self.btnOpen = self.builder.get_object('open')
+        self.btnClear = self.builder.get_object('clear')
+        self.btnSave = self.builder.get_object('save')
+        self.btnInject = self.builder.get_object('inject')
+
+        self.btnOpen.connect('clicked', self.openInEditor)
+        self.btnClear.connect('clicked', self.clearEditor)
+        self.btnSave.connect('clicked', self.save)
+        self.btnInject.connect('clicked', self.inject)
+
+        self.editor_buffer = self.editor.get_buffer()
+
+    def clearEditor(self, btn):
+        self.editor_buffer.set_text('')
+
+    def inject(self, btn):
+        startIter, endIter = self.editor_buffer.get_bounds()
+        TMP = self.editor_buffer.get_text(startIter, endIter, False)
+        ducky.inject_raw(TMP)
+    
+    def openInEditor(self, btn):
+        filechooser = Gtk.FileChooserDialog(title="Open Ducky", parent=None, action=Gtk.FileChooserAction.OPEN)
+        filechooser.add_buttons("_Open", Gtk.ResponseType.OK)
+        filechooser.add_buttons("_Cancel", Gtk.ResponseType.CANCEL)
+        filechooser.set_default_response(Gtk.ResponseType.OK)
+        response = filechooser.run()
+
+        if response == Gtk.ResponseType.OK:
+            try:
+                with open(filechooser.get_filename()) as f:
+                    self.editor_buffer.set_text(f.read())
+            except TypeError:
+                buffer.set_text('Select File First...')
+        filechooser.destroy()
+    
+    def save(self, btn):
+        startIter, endIter = self.editor_buffer.get_bounds()
+        TMP = self.editor_buffer.get_text(startIter, endIter, False)
+        if TMP == '':
+            self.notification('Editor is empty')
+            return
+
+        filechooser = Gtk.FileChooserDialog(title="Open Ducky", parent=None, action=Gtk.FileChooserAction.SAVE)
+        filechooser.add_buttons("_Save", Gtk.ResponseType.OK)
+        filechooser.add_buttons("_Cancel", Gtk.ResponseType.CANCEL)
+        filechooser.set_default_response(Gtk.ResponseType.OK)
+        response = filechooser.run()
+
+        if response == Gtk.ResponseType.OK:
+            try:
+                with open(filechooser.get_filename(), 'w') as f:
+                    f.write(TMP)
+                    self.notification('File Saved')
+            except PermissionError:
+                self.notification('File Not Saved, !!!Access Denied!!!')
+        filechooser.destroy()
+
+class MACChanger(Functions):
+    def __init__(self, builder):
+        self.builder = builder
+        self.maciface = self.builder.get_object('maciface')
+        self.currentmac = self.builder.get_object('currentmac')
+        self.newmac = self.builder.get_object('newmac')
+        self.btnranmac = self.builder.get_object('btn_ranmac')
+        self.btnchmac = self.builder.get_object('btn_chmac')
+        self.btnrstmac = self.builder.get_object('btn_rstmac')
+
+        self.maciface.connect('changed', self.on_iface_change)
+        self.btnranmac.connect('clicked', self.gen_random_mac)
+        self.btnchmac.connect('clicked', self.chmac)
+        self.btnrstmac.connect('clicked', self.reset_mac)
+        self.getifaces()
+
+    def getifaces(self):
+        tmp = psutil.net_if_addrs()
+        iface_list = list(tmp.keys())
+        ifindex = 0 if 'wlan0' not in iface_list else iface_list.index('wlan0')
+        for iface in range(len(iface_list)):
+            self.maciface.append_text(iface_list[iface])
+        self.maciface.set_active(ifindex)
+    
+    def getmac(self, iface):
+        tmp = psutil.net_if_addrs()
+        for i in range(len(tmp[iface])):
+            if 'AddressFamily.AF_PACKET' in str(tmp[iface][i]):
+                return tmp[iface][i][1]
+
+    def on_iface_change(self, iface):
+        curmac = self.getmac(iface.get_active_text())
+        self.currentmac.set_text(curmac)
+    
+    def gen_random_mac(self, btn):
+        mac = ':'.join((f'0{(hex(i)[2:])}' if i<16 else hex(i)[2:]) for i in [random.randint(0, 255) for i in range(6)])
+        self.newmac.set_text(mac)
+    
+    def chmac(self, btn):
+        ifname = self.maciface.get_active_text()
+        newmac = self.newmac.get_text()
+        if len(newmac) == 17:
+            self.get_output(f'ip link set {ifname} down')
+            out = self.get_output(f'macchanger -m {newmac} {ifname}')
+            self.get_output(f'ip link set {ifname} up')
+            if out[1] == 0:
+                self.notification('*MAC CHANGED: *')
+            else:
+                self.notification('!!! MAC NOT CHANGED !!!')
+        self.on_iface_change(self.maciface)
+
+    def reset_mac(self, btn):
+        ifname = self.maciface.get_active_text()
+        self.get_output(f'ip link set {ifname} down')
+        out = self.get_output(f'macchanger -p {ifname}')
+        self.get_output(f'ip link set {ifname} up')
+        if out[1] == 0:
+            self.notification('*MAC RESET DONE: *')
+        else:
+            self.notification('!!! MAC NOT CHANGED !!!')
+        self.on_iface_change(self.maciface)
+
+class NHGUI(Gtk.Application):
+    def __init__(self):
+        Gtk.Application.__init__(self, application_id="in.fossfrog.nh")
+
+    def do_activate(self):
+        appname = 'NetHunter'
+        builder = Gtk.Builder()
+        builder.add_from_file("nethunter.ui")
+
+        # Initialize Functions
+        Arsenal(builder)
+        Ducky(builder)
+        MACChanger(builder)
+
+        # Get The main window from the glade file
+        window = builder.get_object("nh_main")
+        window.set_title(appname)
+        Notify.init(appname)
+
+        # Show the window
+        window.connect("destroy", Gtk.main_quit)
+        window.show_all()
+
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
 
 
-win = MainWindow()
+nh = NHGUI().run(None)
 Gtk.main()
